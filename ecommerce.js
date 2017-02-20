9d48eb28c284c54d811b9a5b88de6f35f05d2907
ritikanagpal4 <ritika.nagpal4@gmail.com>
2017-02-20 18:51:51 +0530

    first commit

---
+++ ecommerce.js
@@ -0,0 +1,255 @@
+var products = [{
+  'name': 'js Prime',
+  'category': 'Electronics',
+  'price': '17000',
+  'discountprice': 16299,
+  'brandname': 'Samsung',
+  'imageurl': [
+    "http://cdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-j7-prime-1.jpg"
+  ]
+}, {
+  'name': 'Desire 620g',
+  'category': 'Electronics',
+  'price': '15000',
+  'discountprice': 13000,
+  'brandname': 'HTC',
+  'imageurl': [
+    "http://trendandtechnology.com/wp-content/uploads/2014/12/151214-101.jpg"
+  ]
+}, {
+  'name': 'Tank Top',
+  'category': 'Women',
+  'price': '1900',
+  'discountprice': 1599,
+  'brandname': 'ZARA',
+  'imageurl': [
+    "https://s-media-cache-ak0.pinimg.com/originals/f0/17/3c/f0173cfd5e11ab7131c7353478a0b3d5.jpg"
+  ]
+}, {
+  'name': 'Bella Belly',
+  'category': 'Women',
+  'price': '1200',
+  'discountprice': 999,
+  'brandname': 'Shoe LAla',
+  'imageurl': [
+    "https://s-media-cache-ak0.pinimg.com/736x/a1/15/17/a1151732cf435b88978f8645ea677560.jpg"
+  ]
+}, {
+  'name': 'Denim Shirt',
+  'category': 'Men',
+  'price': '1800',
+  'discountprice': 1699,
+  'brandname': 'PEOPLE',
+  'imageurl': [
+    "https://2.imimg.com/data2/YL/VD/MY-3042709/dress-shirts-raymond-500x500.jpg"
+  ]
+}, {
+  'name': 'Wrist Band',
+  'category': 'Men',
+  'price': '500',
+  'discountprice': 450,
+  'brandname': 'Joyme',
+  'imageurl': [
+    "http://g02.a.alicdn.com/kf/HTB1nF7pIpXXXXcsXFXXq6xXFXXXM/Joyme-2015-fashion-font-b-men-b-font-font-b-jewelry-b-font-multilayer-leather-charm.jpg"
+  ]
+}, {
+  'name': 'Three Tier Refrigerator',
+  'category': 'Appliances',
+  'price': '15000',
+  'discountprice': 13999,
+  'brandname': 'Samsung',
+  'imageurl': [
+    "https://images-na.ssl-images-amazon.com/images/G/01/th/aplus/samsung/samsung-B00DLD544U-3-l.jpg"
+  ]
+}, {
+  'name': 'Steam Iron',
+  'category': 'Appliances',
+  'price': '1200',
+  'discountprice': 1100,
+  'brandname': 'Phillips',
+  'imageurl': [
+    "https://images.philips.com/is/image/PhilipsConsumer/GC1010_01-RTP-global-001?$jpglarge$&wid=1250"
+  ]
+}, {
+  'name': 'Automatic Washing Machine',
+  'category': 'Appliances',
+  'price': '20000',
+  'discountprice': 18999,
+  'brandname': 'samsung',
+  'imageurl': [
+    "https://image1.pricedekho.com/p/61/1539195/8408099-whirlpool-62-kg-fully-automatic-washing-machine-wm-royale-6512sd-black-chrome-picture-large.jpg"
+  ]
+}, {
+  'name': 'Sports Shoes',
+  'category': 'Men',
+  'price': '1900',
+  'discountprice': 1600,
+  'brandname': 'Puma',
+  'imageurl': [
+    "https://n3.sdlcdn.com/imgs/a/r/a/Puma-White-Sport-Shoes-SDL251890651-1-01a19.JPG"
+  ]
+}, {
+  'name': 'Sling Bag',
+  'category': 'Women',
+  'price': '1000',
+  'discountprice': 899,
+  'brandname': 'SSS',
+  'imageurl': [
+    "http://images1.voonik.com/filters_images/item_type/sling.jpg?vq=7"
+  ]
+}, {
+  'name': '9 to 5 Lipstick',
+  'category': 'Women',
+  'price': '1500',
+  'discountprice': 1400,
+  'brandname': 'Lakme',
+  'imageurl': [
+    "http://www.nykaa.com/media/catalog/product/cache/4/image/300x/9df78eab33525d08d6e5fb8d27136e95/2/2/22936_h_1.jpg"
+  ]
+}, {
+  'name': 'Wrist Watch',
+  'category': 'Men',
+  'price': '12000',
+  'discountprice': 11500,
+  'brandname': 'Casio',
+  'imageurl': [
+    "http://ecx.images-amazon.com/images/I/51fsm8l8rTL._AC_UL260_SR200,260_.jpg"
+  ]
+}, {
+  'name': 'Sandwich Maker',
+  'category': 'Appliances',
+  'price': '1500',
+  'discountprice': 1300,
+  'brandname': 'Phillips',
+  'imageurl': [
+    "https://n2.sdlcdn.com/imgs/a/m/l/NOVA-2-SLICE-PANNI-GRILL-SDL627672213-1-00aea.jpg"
+  ]
+}, {
+  'name': 'Lower Denim',
+  'category': 'Women',
+  'price': '2500',
+  'discountprice': 2200,
+  'brandname': 'Levis',
+  'imageurl': [
+    "http://lsco2.scene7.com/is/image/lsco/women-811-skinny?jpegSize=50&fmt=pjpeg"
+  ]
+}, {
+  'name': 'Lower Denim',
+  'category': 'Men',
+  'price': '2900',
+  'discountprice': 2800,
+  'brandname': 'Levis',
+  'imageurl': [
+    "https://mensjeansguide.files.wordpress.com/2013/04/straight-levis-style-pilot.png"
+  ]
+}, {
+  'name': 'Earphones',
+  'category': 'Electronics',
+  'price': '800',
+  'discountprice': 600,
+  'brandname': 'Sony',
+  'imageurl': [
+    "https://n3.sdlcdn.com/imgs/a/7/j/Sony-MDR-EX150-B-In-SDL697026112-1-a5497.jpg"
+  ]
+}, {
+  'name': 'Macbook Air',
+  'category': 'Electronics',
+  'price': '39000',
+  'discountprice': 37999,
+  'brandname': 'Apple',
+  'imageurl': [
+    "http://brain-images.cdn.dixons.com/7/7/21962977/l_21962977_001.jpg"
+  ]
+}];
+
+
+function ViewProducts(ItemsList) {
+  var mapping = _.map(ItemsList, function(obj) {
+    return "<div><div class='col-md-9 jumbotron'><h2>" +
+      obj.brandname + "<b>  " +
+      obj.name + "</b></h2><h5><i>Actual Price : Rs. <strike>" + obj.price +
+      "</strike></h5>Offer Price:<em> Rs. " +
+      obj.discountprice +
+      "</em></i></div> <div class='col-md-3 jumbotron'> <img src='" + obj.imageurl +
+      "'>"
+    "</div></div>";
+  });
+  //console.log(mapping);
+  $("#outputView").html(mapping);
+};
+
+function filterbybrand() {
+  var selectmenu =
+    "<h3>Filter by</h3><h2> Brand </h2> <br><br><select id='brand_selected'>";
+  var options = (_.map(_.uniqBy(products, 'brandname'), function(o) {
+      return "<option>" + o.brandname;
+    })
+    .join('</option>') + "</option></select>");
+
+  $("#filterdiv").html(selectmenu + options);
+};
+
+function filterbyprice() {
+  var DefineRange =
+    "<h3>Filter by</h3><h2> Price </h2> <br><br>";
+  var inputtypemin = "Min Price: <input type='text' id='min_value'>";
+  var inputtypemax = "Max Price: <input type='text' id='max_value'>";
+  var filterButton =
+    "<br><br><button class='primary' id='pricefilter'>Search</button>"
+  $("#filterdiv").html(DefineRange + inputtypemin + inputtypemax + filterButton);
+};
+
+
+$(document).ready(function() {
+  var heading = (
+    "<div class='container' id='output'> <div class='table-responsive'> <table class='table' border=0><thead><tr>"
+  );
+  window.navcontent = (_.map(_.uniqBy(products, 'category'), function(o) {
+      return "<th><a>" + o.category;
+    })
+    .join('</th> </a>') + "</tr></thead></table></div></div>");
+
+  $("#output").html(heading + navcontent);
+
+  $('#output').on('click', 'a', function() {
+    var text = $(this).text();
+
+    var ItemsList = _.filter(products, {
+      category: text
+    });
+
+    ViewProducts(ItemsList);
+  });
+
+  var FilterHead =
+    "<div class='jumbotron' id= 'filterdiv' style='min-height:400px;'> <h2>Filter </h2><br><br>";
+  var Filters =
+    "<br>Filter by <img style='height:35px;width:45px;' src='https://lh3.googleusercontent.com/aHXrK_QnDjgl624mwaqSqKxW2CGt15IzldrdFC_JO59p77dQTuwsxGOfMqCt_ZNQCLk=w300' onclick='filterbyprice()'><br>" +
+    "<br>Filter By <img style = 'height:35px;width:55px;' src='http://www.einfoway.com/images/brand.png'onclick='filterbybrand()'></div>";
+  $("#filter").html(FilterHead + Filters);
+
+  //filter by brand event
+  $(document).on("change", "#brand_selected", function() {
+    $("select option:selected").each(function() {
+      str = $(this).text();
+
+    });
+    var ItemsList = _.filter(products, {
+      brandname: str
+    });
+
+    ViewProducts(ItemsList);
+  });
+  //filter by price event
+  $(document).on("click", "#pricefilter", function() {
+    var min_value = $("#min_value").val();
+    var max_value = $("#max_value").val();
+    if (min_value && max_value != "") {
+      var ItemsList = _.filter(products, function(o) {
+        return o.discountprice < max_value && o.discountprice >
+          min_value;
+      });
+      ViewProducts(ItemsList);
+    }
+  });
+});
